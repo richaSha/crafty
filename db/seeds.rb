51fe00182c1e824db49589d1b3671fa47db06ed4
richa.shaurbh <richa.shaurabh@gmail.com>
2017-12-05 14:12:28 -0800

    add seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ Image.destroy_all
   crafts = Craft.create!(title: Faker::Name.name,
                 price: Faker::Number.between(1, 20),
                 dcription: Faker::Lorem.sentence(20, false, 0).chop)
+  image = crafts.images.create(photo: open("public/drfault.png"))
 end
 
 p "Created #{Craft.count} crafts"
